195c6ce759e66bcab695ef3c86a277721ff00cfd
AnushaAkkena <120028427+AnushaAkkena@users.noreply.github.com>
2025-12-11 13:42:31 +0530

    Update StartApplication.java

--- src/main/java/com/anusha/StartApplication.java
+++ src/main/java/com/anusha/StartApplication.java
@@ -12,7 +12,7 @@ public class StartApplication {
 
     @GetMapping("/")
     public String index(final Model model) {
-        model.addAttribute("title", "I have successfuly built a sprint boot application using Maven");
+        model.addAttribute("title", "I have successfuly built a sprint boot application using Maven, jenkins");
         model.addAttribute("msg", "welcome to Anusha first project using jenkins, docker, github, sonarqube");
         return "index";
     }
